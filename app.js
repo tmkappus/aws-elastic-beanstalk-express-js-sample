75247900521ac51a2f207f5505dd09cc32441cbd
Kappus <Kappus878@stmary.edu>
2021-09-10 09:52:34 -0500

    change message

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/', (req, res) => res.send('This is fun!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
+
+
